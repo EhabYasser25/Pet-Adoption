b83c787a30eb13768a46db35da8289672da6c40f
Rowaina <rowainaabdelnasser@gmail.com>
2023-12-24 15:00:53 +0200

    .

--- Backend/src/main/java/com/example/Backend/service/StaffRepositoryJDBC.java
+++ Backend/src/main/java/com/example/Backend/service/StaffRepositoryJDBC.java
@@ -2,11 +2,13 @@ package com.example.Backend.service;
 
 import com.example.Backend.config.DatabaseConnection;
 import com.example.Backend.model.Staff;
+import com.example.Backend.repository.StaffRepository;
 
 import java.sql.*;
 
-public class StaffRepositoryJDBC {
+public class StaffRepositoryJDBC implements StaffRepository {
 
+    @Override
     public void addStaff(Staff staff) throws SQLException {
         String sql = "INSERT INTO STAFF (user_id, shelter_id) VALUES (?, ?)";
         try (Connection conn = DatabaseConnection.getConnection();
