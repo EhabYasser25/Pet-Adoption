fcef843b170f74e391ca5b9614d39ac74308a309
mariamgerges575 <96488115+mariamgerges575@users.noreply.github.com>
2023-12-25 15:32:17 +0200

    User home page

---
+++ Frontend/src/NavigationBar.tsx
@@ -0,0 +1,15 @@
+
+
+const NavigationBar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Paws and claws</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">logout</a>
+            </div>
+        </nav>
+    );
+};
+
+export default NavigationBar;
---
+++ Frontend/src/Pages/Applications.tsx
@@ -0,0 +1,8 @@
+
+
+const Applications = () => {
+    return(
+        <h1>  <span className="paw-print">🐾</span>Paws and claws</h1>
+    );
+}
+export default Applications;
