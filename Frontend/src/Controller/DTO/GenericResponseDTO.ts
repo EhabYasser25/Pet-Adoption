7266c61be324808dd1377bcde3f078c90ddb4241
Ehab Yasser <eng.ehabyasser@gmail.com>
2023-12-26 03:09:23 +0200

    initial

---
+++ Frontend/src/Controller/DTO/GenericResponseDTO.ts
@@ -0,0 +1,5 @@
+export type GenericResponseDTO = {
+  status: number
+  message: string
+  data: any
+}
---
+++ Frontend/src/Controller/DTO/authentication/RegistrationRequestDTO.ts
@@ -0,0 +1,9 @@
+export type RegistrationRequestDTO = {
+    email: string
+    userName: string
+    password: string
+    firstName: string
+    lastName: string
+    birthDate: Date
+    nationality: string
+}
