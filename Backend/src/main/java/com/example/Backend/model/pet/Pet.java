8787d5671c6a8fc3a89180243f564852106448e4
mariamgerges575 <96488115+mariamgerges575@users.noreply.github.com>
2023-12-29 16:32:23 +0200

    changes in database

--- Backend/src/main/java/com/example/Backend/model/pet/Pet.java
+++ Backend/src/main/java/com/example/Backend/model/pet/Pet.java
@@ -20,74 +20,14 @@ public class Pet {
     private boolean isSterilized;
     private boolean isVaccinated;
     private boolean isHouseTrained;
-
     private String image;
     private Timestamp releaseTimeStamp;
-
     List<Attachment> attachments;
     private String breed;
-
-    public Pet(int id, int typeId, String name, Date birthDate,
-               Gender gender, boolean isVaccinated, String image, Timestamp releaseTimeStamp, boolean isSterilized, boolean isHouseTrained,String breed) {
-        this.id = id;
-        this.speciesId = typeId;
-        this.name = name;
-        this.birthDate = birthDate;
-        this.gender = gender;
-        this.isVaccinated = isVaccinated;
-        this.image = image;
-        this.releaseTimeStamp = releaseTimeStamp;
-        this.isSterilized = isSterilized;
-        this.isHouseTrained = isHouseTrained;
-        this.breed=breed;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
+    private int shelterId;
+    private int shelterLocationCity;
+    private int shelterLocationCountry;
 
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public boolean isVaccination() {
-        return isVaccinated;
-    }
-
-    public void setVaccination(boolean vaccination) {
-        this.isVaccinated = vaccination;
-    }
-
-    public Timestamp getReleaseTimeStamp() {
-        return releaseTimeStamp;
-    }
-
-    public void setReleaseTimeStamp(Timestamp releaseTimeStamp) {
-        this.releaseTimeStamp = releaseTimeStamp;
-    }
 }
